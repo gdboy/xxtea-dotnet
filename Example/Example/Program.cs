002de6524a3552be0dad34ce4cf7eb3cd3a2143b
llq <664301573@qq.com>
2017-12-12 20:08:26 +0800

    cocos xxtea 资源解密

--- Example/Example/Program.cs
+++ Example/Example/Program.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Xxtea;
 
 namespace Example {
     class MainClass {
         public static void Main (string[] args) {
-            String str = "Hello World! 你好，中国！";
-            String key = "1234567890";
-            String encrypt_data = XXTEA.EncryptToBase64String(str, key);
-            Console.WriteLine(encrypt_data);
-            Debug.Assert("QncB1C0rHQoZ1eRiPM4dsZtRi9pNrp7sqvX76cFXvrrIHXL6" == encrypt_data);
-            String decrypt_data = XXTEA.DecryptBase64StringToString(encrypt_data, key);
-            Debug.Assert(str == decrypt_data);
+
+#if DEBUG
+            var workspace = @"D:\yunding\assets";
+#else
+            var commandLineArgs = Environment.GetCommandLineArgs();
+            var workspace = Path.GetDirectoryName(commandLineArgs[0]);
+#endif
+
+            var files = Directory.GetFiles(workspace, "*.png", SearchOption.AllDirectories);
+
+            foreach(var path in files)
+            {
+                var bytes = File.ReadAllBytes(path);
+
+                bytes = bytes.Skip("YD0888ResPackage".Length).ToArray();
+
+                bytes = XXTEA.Decrypt(bytes, "G9w0BAQefAa0888M");
+
+                if (bytes == null)
+                    continue;
+
+                File.WriteAllBytes(path, bytes);
+            }
         }
     }
 }
